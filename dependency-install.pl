11b36df54315cb1987928c92454b30262d4ea5ae
Elliot Wolk <elliot.wolk@gmail.com>
2015-08-03 12:02:34 -0400

    dependency-install: add script to install klomp and libav-tools

---
+++ dependency-install.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+sub run(@);
+
+my $klompDir = "./klomp";
+
+sub main(@){
+  run "sudo", "apt-get", "install", "libav-tools";
+
+  if(-d $klompDir){
+    chdir $klompDir;
+    run "git", "pull";
+  }else{
+    run "git", "clone", "https://github.com/teleshoes/klomp.git", $klompDir;
+    die "klomp does not exist after git clone\n" if not -d $klompDir;
+    chdir $klompDir;
+  }
+  run "./install.pl";
+}
+
+sub run(@){
+  print "@_\n";
+  system @_;
+  die "Error running \"@_\"\n" if $? != 0;
+}
+
+&main(@ARGV);
